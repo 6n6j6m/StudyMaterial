55941a7f7695866d2cb891067a9a29340bf1f380
Muhammad Najmi Rahmani <najmirahn66@gmail.com>
2026-04-08 00:28:43 +0700

    Initial Commit

---
+++ MyBookShelf/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyBookShelf
+//
+//  Created by Muhammad Najmi Rahmani  on 08/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyBookShelf/MyBookShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBookShelfApp.swift
+//  MyBookShelf
+//
+//  Created by Muhammad Najmi Rahmani  on 08/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MyBookShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
