83dac90735022fc483c2e6fdfa72248c5b9fe60e
Muhammad Najmi Rahmani <najmirahn66@gmail.com>
2026-04-14 22:19:17 +0700

    Belajar walau pake AI

---
+++ MyBookShelf/SwipeCba.swift
@@ -0,0 +1,18 @@
+//
+//  SwipeCba.swift
+//  ProjectIseng
+//
+//  Created by Muhammad Najmi Rahmani  on 14/04/26.
+//
+
+import SwiftUI
+
+struct SwipeCba: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwipeCba()
+}
