7971c04a013877502f8d7a4b5c0d9fb47983b6b7
6n6j6m <najmirahn66@gmail.com>
2026-06-13 18:12:27 +0700

    change: Project Name

---
+++ LearningMaterials/View/StudyMaterialApp.swift
@@ -0,0 +1,18 @@
+//
+//  MyBookShelfApp.swift
+//  MyBookShelf
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct StudyMaterialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        // Menambahkan Model Container untuk SwiftData
+        .modelContainer(for: [StudyMaterial.self])
+    }
+}
